7b7a32d66f9e4b7787a317f619cc1696d39f110e
Aditya Kapoor <adityakapoor270504@gmail.com>
2026-08-08 20:43:13 +0530

    nothing much

--- backend/src/main/java/com/aditya/Main.java
+++ backend/src/main/java/com/aditya/Main.java
@@ -1,8 +1,6 @@
 package com.aditya;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 public class Main {
